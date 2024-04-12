88bb762203a7cd9c0bb12a355bc16baebd55c936
nikhilmehta931 <nikhilmehta931@gmail.com>
2024-04-13 03:12:41 +0530

    helo

---
+++ precedence.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,n;
+    cin>>a>>n;
+    float b= 6.6/a+2*n;
+    cout<<b<<endl;
+
+    float c = 7/22*(3.14+2)*3/5;
+    cout<<c;
+}
